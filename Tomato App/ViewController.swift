c13502818215ea95ec02d9c0b0a489d524a71329
Nurlybakyt <begaly606@gmail.com>
2023-10-24 20:41:15 +0600

    Initial Commit

---
+++ Tomato App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tomato App
+//
+//  Created by Dinmukhammed Begaly on 24.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
